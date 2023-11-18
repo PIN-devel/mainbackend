a4f4d5726aac60de4dec577101fb4b30457bd2b7
Innam Park <pin954562@gmail.com>
2023-11-18 21:57:19 +0900

    mysql

--- mainbackend/settings.py
+++ mainbackend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,12 +74,10 @@ WSGI_APPLICATION = 'mainbackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+with open(BASE_DIR / 'env/db.json') as j:
+    default_db = json.load(j)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': default_db
 }
 
 
